865c4ad3c4fbc4cd58795a35b226885e1bb7b47f
Ninamahdawe <ninamahdawe11@gmail.com>
2023-06-28 21:53:01 -0700

    updated code script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,80 +1,9 @@
 // Assignment Code
 var charactertype = [];
 //var choiceArray = [];
-var lowerCaseArray = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCaseArray = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var specialCharactersArrayy = [
-  "+",
-  "-",
-  "&&",
-  "||",
-  "!",
-  "(",
-  ")",
-  "{",
-  "}",
-  "[",
-  "]",
-  "^",
-  "~",
-  "*",
-  "?",
-  ":",
-];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var specialCharactersArrayy = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":",];
 var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var generateBtn = document.querySelector("#generate");
@@ -87,16 +16,13 @@ function writePassword() {
   passwordText.value = password;
 }
 //prompt the useer//
+// get users desiered password length
+//var con = confirm("Did you choose your password?")
+
 function generatePassword() {
-  // get users desiered password length
   var choiceArray = [];
   var typeCheck = false
-  var characterLength = parseInt(
-    prompt(
-      "Please select the length of your password between 8 and 128 characters"
-    )
-  );
-  //var con = confirm("Did you choose your password?")
+  var characterLength = parseInt(prompt("Please select the length of your password between 8 and 128 characters"));
 
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert(
